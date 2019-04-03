bb32402ccf5d02905f26ac93f350efc2ef47874b
reutenkoIvan <reutenkoivan@gmail.com>
2019-04-03 15:25:42 +0300

    stash changes

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Player from "../components/Player";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Typography from "@material-ui/core/Typography";
 
 configure({ adapter: new Adapter() });
 
@@ -35,19 +36,20 @@ const track = {
     const wrap = shallow(<Player/>);
     const newPlayerInfo = Object.assign({}, playerInfo);
     newPlayerInfo.isPlaying = false;
-
-    wrap.setProps({ newPlayerInfo, track });
+    wrap.setProps({ playerInfo: newPlayerInfo, track });
 
     expect(wrap).toMatchSnapshot()
   });
 
-  //M6 19h4V5H6v14zm8-14v14h4V5h-4z
 
-  it('render with true', () => {
-    const wrap = shallow(<Player/>);
-    const newPlayerInfo = Object.assign({}, playerInfo);
-    newPlayerInfo.isPlaying = true;
-    wrap.setProps({ newPlayerInfo, track });
-
-    expect(wrap).toMatchSnapshot()
-  })
+  // it('render with true', () => {
+  //   const wrap = mount(<Player playerInfo={playerInfo}/>);
+  //   const newPlayerInfo = Object.assign({}, playerInfo);
+  //   newPlayerInfo.isPlaying = true;
+  //   wrap.setProps({ playerInfo: newPlayerInfo, track });
+  //   wrap.update();
+  //   expect(wrap.props().playerInfo.isPlaying).toEqual(true);
+  //   // expect(wrap.find(Typography).findWhere((f) => f.prop('variant') === 'h5')).toHaveLength(1)
+  //   expect(wrap.find({ variant: 'h5'})).toHaveLength(2)
+  //   expect(wrap).toMatchSnapshot()
+  // });
