1a691f830fb902792b687de62577a1d241db4b50
reutenkoivan <reutenkoivan@gmail.com>
2019-04-05 11:37:43 +0300

    added test file for PlayerComponent

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Player from "../components/Player";
-import { mount, shallow } from "enzyme";
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Typography from "@material-ui/core/Typography";
+import { createShallow, getClasses } from '@material-ui/core/test-utils';
+import Pause from "@material-ui/core/SvgIcon/SvgIcon";
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
 
 configure({ adapter: new Adapter() });
-
 const song = {
   artistName: "artistName",
   trackName: "trackName",
@@ -18,38 +19,49 @@ const song = {
   artistViewUrl: "artistViewUrl",
   isFavorite: false
 };
-const playerInfo = {trackLink: "previewUrl",
+const playerInfo = {
+  trackLink: "previewUrl",
   trackId: "trackId",
   artistName: "artistName",
   trackName: "trackName",
   playlist: [song, song],
   index: 1,
   next: 2,
+  isPlaying: false,
   previous: 0
 };
-const track = {
-  songs: [song],
-  favorites: []
-};
 
-  it('renders without crashing', () => {
-    const wrap = shallow(<Player/>);
-    const newPlayerInfo = Object.assign({}, playerInfo);
-    newPlayerInfo.isPlaying = false;
-    wrap.setProps({ playerInfo: newPlayerInfo, track });
+let shallow;
+let classes;
 
-    expect(wrap).toMatchSnapshot()
-  });
+beforeAll(() => {
+  shallow = createShallow({ dive: true });
+  classes = getClasses(<Player />);
+});
 
+it('renders without crashing', () => {
+  const wrap = shallow(<Player playerInfo={playerInfo}/>);
+  expect(wrap).toMatchSnapshot()
+});
 
-  // it('render with true', () => {
-  //   const wrap = mount(<Player playerInfo={playerInfo}/>);
-  //   const newPlayerInfo = Object.assign({}, playerInfo);
-  //   newPlayerInfo.isPlaying = true;
-  //   wrap.setProps({ playerInfo: newPlayerInfo, track });
-  //   wrap.update();
-  //   expect(wrap.props().playerInfo.isPlaying).toEqual(true);
-  //   // expect(wrap.find(Typography).findWhere((f) => f.prop('variant') === 'h5')).toHaveLength(1)
-  //   expect(wrap.find({ variant: 'h5'})).toHaveLength(2)
-  //   expect(wrap).toMatchSnapshot()
-  // });
+it('renders with play button', () => {
+  const wrap = shallow(<Player playerInfo={playerInfo}/>);
+  expect(wrap.find(PlayArrowIcon)).toHaveLength(1);
+  expect(wrap.find({"aria-label": "Pause"})).toHaveLength(0);
+});
+
+it('renders with pause button', () => {
+  const newProps = Object.assign({}, playerInfo);
+  newProps.isPlaying = true;
+
+  const wrap = shallow(<Player playerInfo={newProps}/>);
+  expect(wrap.find(PlayArrowIcon)).toHaveLength(0);
+  expect(wrap.find({"aria-label": "Pause"})).toHaveLength(1);
+});
+
+it('renders with correct title', () => {
+  const wrap = shallow(<Player playerInfo={playerInfo}/>);
+  expect(wrap.find({name: "artistName"})).toHaveLength(1);
+  expect(wrap.find({name: "artistName"}).dive().dive().text()).toEqual(playerInfo.artistName);
+  expect(wrap.find({name: "trackName"}).dive().dive().text()).toEqual(playerInfo.trackName);
+});
