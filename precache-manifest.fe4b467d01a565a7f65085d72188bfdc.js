644866bba5d6a9743a5aa55bc0234cdd23d7c742
reutenkoivan <reutenkoivan@gmail.com>
2019-04-08 09:57:46 +0300

    Updates

---
+++ precache-manifest.fe4b467d01a565a7f65085d72188bfdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "653edffcdca470ee5a87",
+    "url": "/iTunes-demo-searching/static/css/main.df9c16db.chunk.css"
+  },
+  {
+    "revision": "653edffcdca470ee5a87",
+    "url": "/iTunes-demo-searching/static/js/main.9a33058c.chunk.js"
+  },
+  {
+    "revision": "e125c685c56b1fe70b42",
+    "url": "/iTunes-demo-searching/static/js/runtime~main.e7c1b644.js"
+  },
+  {
+    "revision": "4b32d525e7effc4956c3",
+    "url": "/iTunes-demo-searching/static/js/2.24574d77.chunk.js"
+  },
+  {
+    "revision": "094c7df0385da580c9a853b807b71458",
+    "url": "/iTunes-demo-searching/index.html"
+  }
+];
