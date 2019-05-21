ccbf83ef6454188a98bdf793c19a093d1f48a9a8
reutenkoivan <reutenkoivan@gmail.com>
2019-05-21 13:59:42 +0300

    changed favicon

--- src/redux/reducer/favorites.js
+++ src/redux/reducer/favorites.js
@@ -1,7 +1,7 @@
 const favorites = (state = [], action) => {
   switch (action.type) {
     case "ADD_TO_FAVORITE_SUCCESS": {
-      if(typeof action.data != "undefined" && state.every(song => song.trackId - action.data.trackId))
+      if(typeof action.data != "undefined" && state.every(song => song.trackId !== action.data.trackId))
         return [...state, action.data];
 
       return state.filter(song => song.trackId !== action.data.trackId)
